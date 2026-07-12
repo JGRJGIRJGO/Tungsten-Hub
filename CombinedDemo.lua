00dabf6c2bcf6b17a4bd4f013cf940b2b71ca404
JGRJGIRJGO <90160366+JGRJGIRJGO@users.noreply.github.com>
2026-07-11 21:43:48 -0600

    Restore LootLabs key checkpoint flow

--- CombinedDemo.lua
+++ CombinedDemo.lua
@@ -2083,7 +2083,7 @@ local Window = Lyra:CreateWindow({
         Note = "Please enter your secure HWID-locked key to unlock features.",
         SaveKey = true,
         Key = getDailyHWIDKey(),
-        Url = "https://jgrjgirjgo.github.io/Tungsten-Hub/key/?hwid=" .. getHashedHWID()
+        Url = "https://jgrjgirjgo.github.io/Tungsten-Hub/key/?hwid=" .. getHashedHWID() .. "&steps=1&link1=https%3A%2F%2Flootdest.org%2Fs%3FDfJNgxIO"
     }
 })
 
